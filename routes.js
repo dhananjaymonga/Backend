dcb0aa94e3ac3042357a0d5ef071542090f02774
sarikamonga <sarikamonga2306@gmail.com>
2025-02-18 23:19:42 +0530

    ejs staring

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express=require("express")
 const app=express()
+app.set("view engine","ejs");
 app.get("/",(req,res)=>{
     res.send("hi")
 })
@@ -13,9 +14,13 @@ app.get("/:username",(req,res)=>{
     // res.send(req.params)
     console.log(req.query)
     let {username}=req.params
-    let {jiy}=req.query
-    console.log(jiy,username)
-    let httmlstr=`welcome backend@ ${username},${jiy}`
+    let {q}=req.query
+    console.log(q,username)
+    if(!q){
+        res.send("not streing")
+        res.send("<h1>nott</h1>")
+    }
+    let httmlstr=`welcome backend@ ${username},${q}`
     res.send(httmlstr)
 
 })
