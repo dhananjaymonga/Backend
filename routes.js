ca8d999bdf7eb05567cfc2044838b76872028086
sarikamonga <dhananjaymonga@gmail.com>
2025-02-19 00:40:04 +0530

    RENDERING PAGE

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express=require("express")
 const app=express()
-app.set("view engine","ejs");
+const path =require("path")
+app.set("views",path.join(__dirname,"/views"))
+// app.set("view engine","ejs");
 // app.get("/",(req,res)=>{
 //     res.send("hi")
 // })
