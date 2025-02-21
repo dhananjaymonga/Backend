8f911832c9c6323f616f654e0a06f5cd5a68aef1
sarikamonga <dhananjaymonga@gmail.com>
2025-02-21 23:22:48 +0530

    oops start

---
+++ frontend/app.js
@@ -0,0 +1,16 @@
+const app={
+    name : "dhananjay",
+    email:"dhananjaymonga@gmail.com",
+    detail:function(){
+      console.log(this.name ) 
+    }
+}
+const app1={
+    name : "harsh",
+    email:"imverma9921@gmail.com",
+    detail:function(){
+      console.log(this.name ) 
+    }
+}
+
+app.detail()
