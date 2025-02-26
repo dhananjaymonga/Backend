9e82d6176e42c99096dc11cf160befbb5a43c413
Dhananjay Monga <dhananjaymonga@gmail.com>
2025-02-26 17:10:05 +0530

    cookie

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken")
 
 port=4000;
 const cookieparser=require("cookie-parser")
@@ -15,20 +16,7 @@ app.get("/check",(req,res)=>{
     res.send("check cookies")
     })
 
-    // bcrypt
-    app.get("/password",(req,res)=>{
-        bcrypt.genSalt(10,function(err,salt){
-            bcrypt.hash("password",salt,function(err,hash){
-                console.log(hash)
-            })
-        })
-    //  let  salt= bcrypt.genSalt()
-    //   const newpassword=bcrypt.hash("password",salt)
-    //   console.log(newpassword)
-        console.log(req.cookies)
-
-        res.send("check cookies")
-        })
+   
 app.listen(port,()=>{
     
     console.log("start server")
