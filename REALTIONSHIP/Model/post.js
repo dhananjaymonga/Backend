ac5f95e6ab926da8d6fc15860ec76af25e563b6c
dhananjaymonga <dhananjaymonga@gmail.com>
2025-03-08 13:29:08 +0530

    handling deletion

--- REALTIONSHIP/Model/post.js
+++ REALTIONSHIP/Model/post.js
@@ -23,21 +23,36 @@ Like:Number,
    const Post= mongoose.model("Post",postSchema);
 
    const addData= async()=>{
-     const user1= new User({
-       username:"dhananjay",
-       email:"dhananjaymonga@gmail.com",
-      })
-      const post1= new Post({
-        content:"this is post",
-        Like:10,
-        user:user1._id
-      })
-      await user1.save();
-      await post1.save();
-      console.log("data added")
+   //   const user1= new User({
+   //     username:"dhananjay",
+   //     email:"dhananjaymonga@gmail.com",
+   //    })
+   //    const post1= new Post({
+   //      content:"this is post",
+   //      Like:10,
+   //      user:user1._id
+   //    })
+   //    const post2= new Post({
+   //      content:"by by",
+   //      Like:10,
+   //      user:user1._id
+   //    })
+   //    await user1.save();
+   //    await post1.save();
+   //    await post2.save();
+   //    console.log("data added")
+  //  const result =await Post.find().populate("user");
+  //  console.log(result)
+  //  }
+  //  addData().then(()=>{
+  //    console.log("data added")
+  //  }).catch((err)=>{
+  //    console.log(err)
    }
-   addData().then(()=>{
-     console.log("data added")
-   }).catch((err)=>{
-     console.log(err)
-   })
+
+   
+
+
+
+
+         
--- REALTIONSHIP/Model/userdetails.js
+++ REALTIONSHIP/Model/userdetails.js
@@ -18,22 +18,21 @@ const orderSchema = new Schema({
     price: Number,
 });
 
-// Create Order Model
 const Order = mongoose.model("Order", orderSchema);
 
 // Function to Insert Order Details
-const orderDetails = async () => {
-    try {
-        await Order.insertMany([
-            { item: "laptop", price: 50000 },
-            { item: "mobile", price: 20000 },
-            { item: "tv", price: 30000 },
-        ]);
-        console.log("Orders inserted successfully");
-    } catch (err) {
-        console.error("Error inserting orders:", err);
-    }
-};
+// const orderDetails = async () => {
+//     try {
+//         await Order.insertMany([
+//             { item: "laptop", price: 50000 },
+//             { item: "mobile", price: 20000 },
+//             { item: "tv", price: 30000 },
+//         ]);
+//         console.log("Orders inserted successfully");
+//     } catch (err) {
+//         console.error("Error inserting orders:", err);
+//     }
+// };
 
 // Define User Schema
 const userSchema = new Schema({
@@ -44,37 +43,72 @@ const userSchema = new Schema({
     }]
 });
 
-// Create User Model
+// userSchema.pre("findOneAndDelete", async()=> {
+// console.log("Pre middleware")
+// })
+userSchema.post("findOneAndDelete",  async function(coustomer){     
+    if(coustomer.orders.length){
+        const result = await Order.deleteMany({_id:{$in:coustomer.orders}})
+        console.log(result)
+    }
+console.log("Post middleware")
+})
+
 const User = mongoose.model("User", userSchema);
 
 // Function to Insert User Details
-const userDetails = async () => {
+// const userDetails = async () => {
+//     try {
+//         // Ensure orders are inserted first
+//         await orderDetails();
+
+//         // Find orders
+//         let order1 = await Order.findOne({ item: "laptop" });
+//         let order2 = await Order.findOne({ item: "mobile" });
+
+//         if (!order1 || !order2) {
+//             console.log("Orders not found!");
+//             return;
+//         }
+
+//         // Create new user and add order ObjectIds
+//         const user = new User({
+//             username: "dhananjay",
+//             orders: [order1._id, order2._id]  // <-- FIXED
+//         });
+
+//         // Save user
+//         let result = await user.save();
+//         console.log("User saved successfully:", result);
+//     } catch (err) {
+//         console.error("Error inserting user:", err);
+//     }
+// };
+
+// // Call function
+// userDetails();
+
+
+// const addCustomer = async () => {
+// const newCustomer = new User({
+//     username: "dhananjay",
+    
+// })
+// const newOrder = new Order({
+//     item: "laptop",
+//     price: 50000,
+// })
+// newCustomer.orders.push(newOrder);
+// let result = await newCustomer.save();
+// // console.log(result)
+// }
+// addCustomer().then(() => {console.log("data svaed")}
+// ).catch((err) => {console.log(err)})
+
+deleteCustomer = async () => {
     try {
-        // Ensure orders are inserted first
-        await orderDetails();
+        let result = await User.findByIdAndDelete("67cbf02ba23703a6491099d8" );
+        console.log("User deleted successfully:", result);      } catch (err) { console.log(err)}
 
-        // Find orders
-        let order1 = await Order.findOne({ item: "laptop" });
-        let order2 = await Order.findOne({ item: "mobile" });
-
-        if (!order1 || !order2) {
-            console.log("Orders not found!");
-            return;
-        }
-
-        // Create new user and add order ObjectIds
-        const user = new User({
-            username: "dhananjay",
-            orders: [order1._id, order2._id]  // <-- FIXED
-        });
-
-        // Save user
-        let result = await user.save();
-        console.log("User saved successfully:", result);
-    } catch (err) {
-        console.error("Error inserting user:", err);
     }
-};
-
-// Call function
-userDetails();
+    deleteCustomer().then(() => { console.log("data deleted") }).catch((err) => { console.log(err) })
